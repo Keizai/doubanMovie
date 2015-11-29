9d084f67d76c8fc11cbfc98dd5b7bd7ab9ec8946
Keizai <36269335>
2015-11-29 16:31:00 +0800

    Initial Commit

---
+++ doubanMovie/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  doubanMovie
+//
+//  Created by 赵琦 on 15/11/29.
+//  Copyright © 2015年 赵琦. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ doubanMovie/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  doubanMovie
+//
+//  Created by 赵琦 on 15/11/29.
+//  Copyright © 2015年 赵琦. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
